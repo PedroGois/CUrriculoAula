02985286f2c62458a2cff4dd7c61faa7b6b3bfaa
PedroGois <pedromorais.gois@gmail.com>
2023-11-08 20:44:00 -0300

    era

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menuIcon');
+    const menuList = document.getElementById('menuList');
+  
+    menuIcon.addEventListener('click', function () {
+      menuList.classList.toggle('active');
+    });
+  });
